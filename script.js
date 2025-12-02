005f8538e579ad7e0a4a10f737f6bc96c89ff049
mithileshmaddhesia <mithileshmaddheshiya123@gmail.com>
2025-12-02 20:50:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+
+// testimonials
+
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 60,
+        },
+      },
+    });
